cfc679e33a5252c16ae4712b748defd16428590e
paulina_emanowicz <emanowiczpaulina@gmail.com>
2024-12-13 18:21:30 +0100

    Dodano plik 3_Python_pd4655_PYTHON_13.py

---
+++ 3_Python_pd4655_PYTHON_13.py
@@ -0,0 +1,37 @@
+# biologia.py
+from collection import Counter
+
+def opis_komorki():
+    return "Komorka to podstawowa jednostka zycia"
+
+def licz_nukleotydy(sekwencja):
+    return Counter(sekwencja)
+
+
+# Glowny program
+import os
+import datetime
+import biologia
+
+# Tworzenie katalogu "dane_bio"
+katalog = "dane_bio"
+if not os.path.exist(katalog)
+   os.mkdir(katalog)  
+   print(f"Katalog '{katalog}' zostal utworzony.")
+
+# przygotowanie danych
+sekwencja_dna = "AGCTTAGCTAAGGCT"
+wynik_nukleotydy = biologia.licz_nukleotydy(sekwencja_dna)
+data_utworzenia = datetime.datetime.now()
+
+# Tworzenie pliku i zapisanie danych 
+plik_sciezka = os.path.join(katalog, "nukleotydy.txt")
+with open(plik_sciezka, "w") as plik:
+    plik.write("Wynik funckji licz_nukleotydy:\n")
+    for nukleotyd, liczba in wynik_nukleotydy.items():
+        plik.write(f"{nukleotyd}: {liczba}\n")
+    plik.write(f"\nData utworzenia pliku: {data_utworzenia}\n")
+print(f"Plik '{plik_sciezka}' został utworzony i zapisany.")
+
+# Wyświetlenie wyniku funkcji opis_komorki
+print(biologia.opis_komorki())
