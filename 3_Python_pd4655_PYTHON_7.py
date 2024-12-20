0744b18dc432f5eb50ae47cf9baaf83e5b88b6f1
paulina_emanowicz <emanowiczpaulina@gmail.com>
2024-12-20 17:55:38 +0100

    .

--- 3_Python_pd4655_PYTHON_7.py
+++ 3_Python_pd4655_PYTHON_7.py
@@ -1,51 +1,28 @@
-# 1 Zadeklarowanie zmiennych typu boolean
-czy_sekwencja_dna_aktywna = True
-czy_mutacja_punktowa = False
+# 1 definicja zmiennych 
+sekwencja_dna1 = "ATCGGCTA"
+sekwencja_dna2 = "GCTAGTCA"
 
-print(f"Czy sekwencja DNA aktywna? {czy_sekwencja_dna_aktywna}")
-print(f"Czy mutacja punktowa? {czy_mutacja_punktowa}")
+# 2 łączenie dwóch zmiennych
+polaczona_sekwencja = sekwencja_dna1 + sekwencja_dna2
+print("Polaczona sekwencja DNA:", polaczona_sekwencja)
 
+# 3 wycinanie fragmentów za pomocą string slicing
+fragment1 = polaczona_sekwencja[:8]
+fragment2 = polaczona_sekwencja[4:12]
+print("fragment1(pierwsze 8 nukleotydow):", fragment1)
+print("fragment2(od 5. do 12. nukleotydów):",fragment2)
 
-# 2 Sprawdzanie zmiennych z użyciem funkcji bool()
-ciag_znakow = "ATCG"
-pusta_lista = []
-liczba = 0
+# 4 analiza sekwencji 
+liczba_atcg = polaczona_sekwencja.count("ATCG")
+pozycja_gcta = polaczona_sekwencja.find("GCTA")
+print("Liczba wystapien 'ATCG':",liczba_atcg)
+print("Pozycja 'GCTA':", pozycja_gcta)
 
-print(f"Czy ciąg znaków nie jest pusty? {bool(ciag_znakow)}")
-print(f"Czy lista jest pusta? {bool(pusta_lista)}") 
-print(f"Czy liczba jest różna od zera? {bool(liczba)}")
-
-
-# 3 Operacje arytmetyczne
-liczba_nukleotydow = 256  
-liczba_kodonow = liczba_nukleotydow // 3  
-reszta_nukleotydow = liczba_nukleotydow % 3
-
-print(f"Liczba kodonów: {liczba_kodonow}")
-print(f"Reszta nukleotydów: {reszta_nukleotydow}")
-
-suma = liczba_nukleotydow + 44
-roznica = liczba_nukleotydow - 100
-iloczyn = liczba_nukleotydow * 2
-
-print(f"Suma: {suma}")
-print(f"Różnica: {roznica}")
-print(f"Iloczyn: {iloczyn}")
+# 5 Formatowanie
+formatted_output = f"Sekwencja:\n\t{polaczona_sekwencja}\nFragmenty:\n\t1. {fragment1}\n\t2. {fragment2}"
+print(formatted_output)
 
 
-# 4 Operatory przypisania
-liczba_nukleotydow += 44 
-liczba_nukleotydow -= 100  
-print(f"Aktualna liczba nukleotydów: {liczba_nukleotydow}")
-
-
-# 5 Operatory porównania
-dlugosc_sekwencji1 = 200
-dlugosc_sekwencji2 = 200
-
-if dlugosc_sekwencji1 < dlugosc_sekwencji2:
-    print("Sekwencja 2 jest dłuższa od sekwencji 1")
-elif dlugosc_sekwencji1 > dlugosc_sekwencji2:
-    print(f"Sekwencja 1 jest dłuższa od sekwencji 2")
-else:
-    print(f"Obie sekwencje mają taką samą długość")
+# 6 Znak ucieczki 
+sekwencja_zmieniona = polaczona_sekwencja.replace("GCTA", "XXXX")  
+print("\nZmieniona sekwencja po zamianie 'GCTA' na 'XXXX':", sekwencja_zmieniona)
