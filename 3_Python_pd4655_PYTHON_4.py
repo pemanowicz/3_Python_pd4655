fec399ed6c9a548e5fa254d07432b39a5d474df3
paulina_emanowicz <emanowiczpaulina@gmail.com>
2024-12-20 17:46:20 +0100

    Wprowadzono zmiany w zadaniu 4

--- 3_Python_pd4655_PYTHON_4.py
+++ 3_Python_pd4655_PYTHON_4.py
@@ -1,15 +1,15 @@
-# Tworzenie zmiennych o różnych typach 
-nazwa_genotypu = 'BRCA1' # Zmienna tekstowa (str)
-liczba_mutacji = 3 # Zmienna liczby całkowitej (int)
-masa_bialka = 58.44 # Zmienna liczby zmiennoprzecinkowej (float)
+# zmiennee o różnych typach 
+nazwa_genotypu = 'BRCA1' (str)
+liczba_mutacji = 3 (int)
+masa_bialka = 58.44 (float)
 
-# Wyświetlanie typów zmiennych 
+# wyświetlanie typów zmiennych 
 print(type(nazwa_genotypu))
 print(type(liczba_mutacji))
 print(type(masa_bialka))
 
 
-# Wyświetlanie wartości zmiennych w jednej linii
+# wyświetlanie wartości zmiennych w jednej linii
 print('Genotyp:', nazwa_genotypu, 'Liczba mutacji:', liczba_mutacji, 'Masa bialka:', masa_bialka)
 
 # Dodawanie liczb i wyświetlanie wyniku
@@ -17,6 +17,6 @@ dodatkowe_mutacje = 2
 suma_mutacji = liczba_mutacji + dodatkowe_mutacje 
 print('Suma mutacji:', suma_mutacji)
 
-# Łączenie tekstu i wyświetlanie 
+# lączenie tekstu i wyświetlanie 
 opis = nazwa_genotypu + " jest ważnym genem supresorowym nowotworów."
 print(opis)
